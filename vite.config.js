8b38f09b4553c4c8f16b7b6e11a1a9dbdf22ba2e
Miguelrenearana <miguelreneas@gmail.com>
2026-05-13 02:53:39 -0400

    todo las configs del frot anadidas al viteconfig,js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/css/estilos.css','public/js/scrip.js' ],
+            input: ['public/css/auth/login.css','public/css/compartido/topbar.css','public/css/dashboard/dashboard.css','public/css/dashboard_usuario.css','public/css/dashboard.css','public/css/estilos.css','public/js/auth/login.js','public/js/compartido/topbar.js','public/js/scrip.js','public/js/validaciones.js'],
             refresh: true,
         }),
         tailwindcss(),
