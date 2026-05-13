69dac40d534a7acff6742ecea39b7bf3007deaa0
Miguelrenearana <miguelreneas@gmail.com>
2026-05-13 02:07:49 -0400

    llenando el input con js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [],
+            input: [resources/js/app.js],
             refresh: true,
         }),
         tailwindcss(),
